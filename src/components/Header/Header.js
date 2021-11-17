019bbb05a819325099a6aa34604ca96bbe7d2bc8
Remi Marchand <remievan@hotmail.com>
2021-11-17 16:51:50 -0500

    Build the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,8 +10,30 @@ import Button from '../Button';
 
 const Header = () => {
   return (
-    <header>
-      <SuperHeader>
+    <>
+      <MobileHeader>
+        <SuperHeader>
+          <Row>
+            <ActionGroup>
+              <button>
+                <Search size={24} />
+              </button>
+              <button>
+                <Menu size={24} />
+              </button>
+            </ActionGroup>
+            <ActionGroup>
+              <button>
+                <User size={24} />
+              </button>
+            </ActionGroup>
+          </Row>
+        </SuperHeader>
+        <MainHeader>
+          <Logo />
+        </MainHeader>
+      </MobileHeader>
+      <DesktopHeader>
         <Row>
           <ActionGroup>
             <button>
@@ -21,19 +43,36 @@ const Header = () => {
               <Menu size={24} />
             </button>
           </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
+          <MainHeader>
+            <Logo />
+          </MainHeader>
+          <Column>
+            <Button>
+              Subscribe
+            </Button>
+            <Link href="somewhere">
+              Already a subscriber?
+            </Link>
+          </Column>
         </Row>
-      </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
-    </header>
+      </DesktopHeader>
+    </>
   );
 };
+
+const MobileHeader = styled.header`
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopHeader = styled.header`
+  display: none;
+  
+  @media ${QUERIES.desktopAndUp} {
+    display: revert;
+  }
+`
 
 const SuperHeader = styled.div`
   padding: 16px 0;
@@ -44,7 +83,19 @@ const SuperHeader = styled.div`
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
+  align-items: baseline;
+  
+  @media ${QUERIES.desktopAndUp} {
+    justify-content: revert;
+  }
 `;
+
+const Column = styled.div`
+  display: flex;
+  gap: 8px;
+  flex-direction: column;
+  align-items: center;
+`
 
 const ActionGroup = styled.div`
   display: flex;
@@ -65,6 +116,21 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+  
+  @media ${QUERIES.desktopAndUp} {
+    margin-right: auto;
+    margin-left: auto;
+  }
+`;
+
+const Link = styled.a`
+  text-decoration: underline;
+  font-style: italic;
 `;
 
 export default Header;
